46559000f1879bacb36e4a8bbd34a81cff5d8ca4
Nat <nat@cmmc.io>
2019-06-10 17:29:32 +0700

    add kbx.

---
+++ template.c
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <vector>
+#include <WiFi.h>
+
+${EXTINC}
+
+
+
+typedef int Number;
+typedef int Boolean;
+
+using namespace std;
+
+${VARIABLE}
+
+${FUNCTION}
+
+void setup()
+{
+
+  ${SETUP_CODE}
+  ${BLOCKSETUP}
+}
+void loop()
+{
+  ${LOOP_CODE}
+  ${LOOP_EXT_CODE}
+}
