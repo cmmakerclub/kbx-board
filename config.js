43108400dc9ad8ac012d25a4bf9993e55a9103cc
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-12-12 16:46:31 +0700

    update readme

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ module.exports = {
   email: "makerasia@gmail.com",
   git: "https://github.com/cmmakerclub/kbx-board/",
   image: "/static/display.jpg",
-  version: "1.1.3",
+  version: "1.1.4",
 };
